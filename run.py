1a837ca8e9ff1dfe0a1270111143ca5d1afd42c3
Junho Yeo <hanaro0704@gmail.com>
2020-09-26 20:17:24 +0900

    feat: add delay between requests

--- run.py
+++ run.py
@@ -1,13 +1,15 @@
+from time import sleep as delay
 from pprint import pprint
 from get_drawable_items import get_drawable_items
 from get_calendar_for_item import get_calendar_for_item
 
 if __name__ == '__main__':
   drawable_items = get_drawable_items()
-  pprint(drawable_items)
+  delay(0.1)
 
   for item in drawable_items:
     calendar = get_calendar_for_item(item['href'])
     item['calendar'] = calendar
+    delay(0.25)
 
   pprint(drawable_items)
