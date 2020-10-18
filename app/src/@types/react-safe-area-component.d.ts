f25ab5a22742c49e17990efc995ff64f82dd2127
Junho Yeo <hanaro0704@gmail.com>
2020-10-18 20:41:28 +0900

    feat: wrap with SafeArea

---
+++ app/src/@types/react-safe-area-component.d.ts
@@ -0,0 +1,1 @@
+declare module 'react-safe-area-component';
--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import SafeArea from 'react-safe-area-component';
 import styled from 'styled-components';
 
 import CalendarCard from './components/CalendarCard';
@@ -8,22 +9,22 @@ import sneakers from './mockups/sneakers.json';
 
 const App: React.FC = () => {
   return (
-    <ServiceWrapper>
-      <Container>
-        {sneakers.map((sneaker, index) => (
-          <CalendarCard key={`sneaker-${index}`} {...sneaker} />
-        ))}
-      </Container>
-    </ServiceWrapper>
+    <SafeArea top bottom>
+      <ServiceWrapper>
+        <Container>
+          {sneakers.map((sneaker, index) => (
+            <CalendarCard key={`sneaker-${index}`} {...sneaker} />
+          ))}
+        </Container>
+      </ServiceWrapper>
+    </SafeArea>
   );
 };
 
 export default App;
 
 const Container = styled.div`
+  padding: 16px 0;
   width: 100%;
   height: 100%;
-  padding-top: 16px;
-  padding-bottom: calc(constant(safe-area-inset-bottom) + 16px);
-  padding-bottom: calc(env(safe-area-inset-bottom) + 16px);
 `;
