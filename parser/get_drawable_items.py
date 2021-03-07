a3ea5bd8921fb301f0492bf688f4c23fb3259f2f
Junho Yeo <hanaro0704@gmail.com>
2021-03-07 11:40:10 +0900

    fix: update selectors for parser

--- parser/get_drawable_items.py
+++ parser/get_drawable_items.py
@@ -5,19 +5,23 @@ def get_drawable_items():
     soup = api.get_request(
         '/kr/launch?type=upcoming&activeDate=date-filter:AFTER')
 
-    launch_items = soup.find_all('a', class_='comingsoon')
+    launch_items = soup.find_all('div', class_='product-card')
+    # print(launch_items)
     drawable_items = []
 
     for launch_item in launch_items:
-        soldout_button = launch_item.find('span', class_='btn-soldout')
-        if (soldout_button and soldout_button.text == 'THE DRAW 진행예정'):
+        soldout_button = launch_item.find('a', class_='ncss-btn-primary-dark')
+        print(soldout_button)
+        if (soldout_button and soldout_button.text.strip() == 'THE DRAW 진행예정'):
+            launch_item_information = launch_item.find(
+                'a', class_='comingsoon')
             launch_item_image = launch_item.find('img', class_='img-component')
 
             drawable_items.append({
-                'title': launch_item.attrs['title'],
+                'title': launch_item_information.attrs['title'],
                 'theme': launch_item_image.attrs['alt'],
                 'image': launch_item_image.attrs['data-src'],
-                'href': launch_item.attrs['href'],
+                'href': launch_item_information.attrs['href'],
             })
 
     return drawable_items
