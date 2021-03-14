aee7e5b87eef65ed521bb71d48a16d9b605895e7
Junho Yeo <hanaro0704@gmail.com>
2021-03-14 23:33:29 +0900

    feat: add empty message for app

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -12,6 +12,12 @@ const App: React.FC = () => {
     <>
       <ServiceWrapper>
         <Container>
+          {sneakers.length === 0 && (
+            <EmptyText>
+              진행중인 드로우가 없습니다!<br />
+              <span>No available draws at the time</span>
+            </EmptyText>
+          )}
           {sneakers.map((sneaker, index) => (
             <CalendarCard key={`sneaker-${index}`} {...sneaker} />
           ))}
@@ -29,3 +35,19 @@ const Container = styled.div`
   width: 100%;
   height: 100%;
 `;
+
+const EmptyText = styled.h1`
+  margin-top: 32px;
+  text-align: center;
+  font-size: 24px;
+  line-height: 1.4;
+
+  @media screen and (max-width: 450px) {
+    font-size: 18px;
+  }
+
+  span {
+    font-size: 85%;
+    color: rgba(0, 0, 0, 0.65);
+  }
+`
