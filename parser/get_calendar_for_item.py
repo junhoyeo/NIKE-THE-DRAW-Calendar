8f265db84b7a7345ea8ef7995a155e1b451e00d5
LeeSeongJinCa <dltjdqhr55@gmail.com>
2021-04-15 10:46:55 +0900

    change class of description_wrapper from soup

--- parser/get_calendar_for_item.py
+++ parser/get_calendar_for_item.py
@@ -4,10 +4,9 @@ import api
 def get_calendar_for_item(item_href):
     soup = api.get_request(item_href)
 
-    description_wrapper = soup.find('p', class_='text-bold')
+    description_wrapper = soup.find_all('p', class_='draw-info')
     calendar = [
-        paragraph.text
-        for paragraph in description_wrapper.find_all('p')
+        paragraph.text for paragraph in description_wrapper
     ]
 
     return calendar
